d739dca810c31f8bb0df45af3a7d764bed480fb8
dominx99 <dominx992@gmail.com>
2021-12-27 15:15:36 +0100

    Add possibility to add new transaction

--- src/main/java/pl/borecki/wallet/transaction/TransactionController.java
+++ src/main/java/pl/borecki/wallet/transaction/TransactionController.java
@@ -1,6 +1,10 @@
 package pl.borecki.wallet.transaction;
 import java.util.List;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +20,22 @@ public class TransactionController {
         Iterable<Transaction> transactions = transactionRepository.findAll();
 
         return transactions;
+    }
+
+    @PostMapping(value = "/api/transactions")
+    public String store(
+        @RequestParam String name,
+        @RequestParam Integer value,
+        @RequestParam String type
+    ) {
+        Transaction transaction = new Transaction();
+
+        transaction.setName(name);
+        transaction.setType(type);
+        transaction.setValue(value);
+
+        transactionRepository.save(transaction);
+
+        return "OK";
     }
 }
