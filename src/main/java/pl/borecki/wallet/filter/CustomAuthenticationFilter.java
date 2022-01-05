cc6e02c667c988996b73494f82d2e5630c5d309f
michalboro <michalborecki5@gmail.com>
2022-01-05 20:42:55 +0100

    create access_token & refresh_token

--- src/main/java/pl/borecki/wallet/filter/CustomAuthenticationFilter.java
+++ src/main/java/pl/borecki/wallet/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package pl.borecki.wallet.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +16,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
+
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     private final AuthenticationManager authenticationManager;
@@ -30,8 +37,22 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) //mozna dodac unsuccessful //to potrzebne do generowania tokena
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) //mozna dodac unsuccessful //to potrzebne do generowania tokena
             throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        User user  = (User) authentication.getPrincipal();
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); //potrzebna dependencja auth0
+        String access_token = JWT.create()
+                .withSubject(user.getUsername()) // token bedzie zwracal ukrytego usernama
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 10*60*1000)) // ustawiony na 10 minut
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .sign(algorithm);
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+ 60*60*1000)) // ustawiony na godzine
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+        response.setHeader("access_token", access_token);
+        response.setHeader("refresh_token", refresh_token);
     }
 }
